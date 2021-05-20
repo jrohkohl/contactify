e842139a8b4dce0ec6956e0a2531a41d9fe8c0bf
jrohkohl <jrohkohl@chicagobooth.edu>
2021-05-20 14:30:25 +0000

    added root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  
+  root "contacts#index"
+  
+  devise_for :users
+
   resources :members
   resources :groups
   resources :contacts
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  
 end
