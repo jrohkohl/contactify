8110f191ede1d932e77f52c6c57eeef71e1d4df9
jrohkohl <jrohkohl@chicagobooth.edu>
2021-05-20 15:01:20 +0000

    set user show action

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,15 @@ Rails.application.routes.draw do
   
   root "contacts#index"
   
+  
+
   devise_for :users
 
   resources :members
   resources :groups
   resources :contacts
+  
+  resources :users, only: :show
   
   
 end
